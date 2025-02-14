6a0c692ebdafc6166e92df31aee20a14d2e04058
Jonesey86 <irj082024@gmail.com>
2025-02-14 09:38:48 +0100

    changes

--- obj/Debug/net9.0/Login System.AssemblyInfo.cs
+++ obj/Debug/net9.0/Login System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e9bb5971b02c36f063e06e32da99ac80d22184")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3830d75abe4db12b981a31b8adfc8b57e582f79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
